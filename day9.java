3b2b57b16f1b3490f8b90cd4e6ce41c814c1af55
Manav Rajput <111671746+rajputmanav540@users.noreply.github.com>
2025-02-22 22:01:35 +0530

    Create day9.java

---
+++ day9.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1; // Pointer for nums1
+        int p2 = n - 1; // Pointer for nums2
+        int p = m + n - 1; // Pointer for the merged array in nums1
+
+        // Merge from the end
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+
+        // If elements remain in nums2, copy them
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+    }
+}
+
